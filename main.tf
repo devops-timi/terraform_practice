a3cc57627730c258c1078da14e79a662d4a527ac
devops-timi <tobalereko@gmail.com>
2025-10-15 22:51:04 +0100

    Made changes to main terraform file

--- main.tf
+++ main.tf
@@ -6,30 +6,14 @@ terraform {
     }
   }
 }
-
+How to write output.tf and variable.tf and other .tf files?
 provider "aws" {
-  region = "eu-north-1"
-}
-
-# Automatically fetch latest Ubuntu 22.04 LTS (Jammy Jellyfish)
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical's official AWS account ID
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  region = var.region
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "Terraform_Demo"
