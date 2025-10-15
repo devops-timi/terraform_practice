df2816b91081fc6eecc20ea72e93484dbb47bbdf
devops-timi <tobalereko@gmail.com>
2025-10-15 22:44:05 +0100

    Created data terraform variable

---
+++ data.tf
@@ -0,0 +1,15 @@
+# Automatically fetch latest Ubuntu 22.04 LTS (Jammy Jellyfish)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
